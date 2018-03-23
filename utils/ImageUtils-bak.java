84f08afd66c6b4b1c6877182594e444559dd57d8
hell007 <1048523306@qq.com>
2018-03-23 17:20:25 +0800

    Update ImageUtils-bak.java

--- utils/ImageUtils-bak.java
+++ utils/ImageUtils-bak.java
@@ -23,99 +23,88 @@ public class ImageUtils {
 	
 	private static Logger logger = LoggerFactory.getLogger(ImageUtils.class);
 	
-     
+	//根据配置文件获取服务器图片存放路径前缀
+	private static String IMAGE_PREFIX = PropertiesUtils.getFileIO("savePicUrl", "properties/config.properties");
+	
+
 	/**
 	 * 功能描述   文件类型判断
-	 * @param fileName
+	 * @param filename
 	 * @param allowType => image flash media file
 	 * @return
 	 */
-	public static boolean isAllowFile(String fileName,String allowType) {  
+	public static boolean isAllowFile(String filename,String allowType) {  
 		// 定义允许上传的文件扩展名
 		Map<String, String> extMap = new HashMap<String, String>();
 		extMap.put("image", "gif,jpg,jpeg,png,bmp");
 		extMap.put("flash", "swf,flv");
 		extMap.put("media", "swf,flv,mp3,wav,wma,wmv,mid,avi,mpg,asf,rm,rmvb");
 		extMap.put("file", "doc,docx,xls,xlsx,ppt,htm,html,txt,zip,rar,gz,bz2");
-		boolean isAllow = false;
 		// 检查扩展名
-		String fileExt = fileName.substring(fileName.lastIndexOf(".") + 1).toLowerCase();
-		if (Arrays.<String> asList(extMap.get(allowType).split(",")).contains(fileExt)) {
-			isAllow = true;
-		} 
-        return isAllow;  
+		String fileExt = filename.substring(filename.lastIndexOf(".") + 1).toLowerCase();
+		if (Arrays.<String> asList(extMap.get(allowType).split(",")).contains(fileExt)) return true; 
+        return false;  
     }
 	
 	
 	/**
-	 * 功能描述   保存图片 
-	 * @param filePath 保存文件的子目录
-	 * @param file    文件文件
-	 * @param isThumb
-	 * @return 返回文件存放信息	
+	 * 功能描述   保存图片
+	 * @param path 保存图片的子目录
+	 * @param file springMVC的MultipartFile
+	 * @param bool 是否生成缩略图 true 是  false 否
+	 * @return
 	 */
-	public static Map<String,String> saveFile(String filePath, MultipartFile file, boolean isThumb) {	   	
-    	// 根据配置文件获取服务器图片存放路径
-        String saveFilePath = PropertiesUtils.getFileIO("savePicUrl", "properties/config.properties");
-
-        // 构建文件目录 
-        File fileDir = new File(new File(saveFilePath), filePath);
+	public static Map<String,String> saveImage(String path, MultipartFile file, boolean bool) {		
+		// 构建文件目录 ，检查目录写权限
+        File fileDir = new File(new File(IMAGE_PREFIX), path);
         if (!fileDir.exists()) {fileDir.mkdirs();}
-        // 检查目录写权限
  		if (!fileDir.canWrite()) {return null;}	
-        // 构建源图文件名
-        String filename = filePath + DateUtils.formatDate(new Date(), "yyyyMMddhhmmss")
-    			+ ((int)(Math.random()*(99999-10000)+10000)) + "." 
-        		+ file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")+1);
-        String result = saveFilePath+filename;
-        
-        Map<String,String> fileMap = new HashMap<String,String>();
+ 		
+        Map<String,String> result = new HashMap<String,String>();
         //生成缩略图并进行图片压缩
-        // S:100 M:400 B:800
-        if(isThumb){
-        	String thumb4 = generateFixedSizeImage(file,result,400,400,0.8);
-        	if(thumb4!=null){fileMap.put("thumbM", thumb4);}
-        	String thumb1 = generateFixedSizeImage(file,result,100,100,0.8);
-        	if(thumb1!=null){fileMap.put("thumbS", thumb1);}
-        	String thumb8 = generateFixedSizeImage(file,result,800,800,0.8);      	
-        	if(thumb8!=null){fileMap.put("thumbB", thumb8);}
+        if(bool){
+        	MultipartFile temp = file;
+        	result.put("small", generateFixedSizeImage(temp,path,100,100,0.8));
+        	result.put("medium", generateFixedSizeImage(temp,path,400,400,0.8));
+        	result.put("source", generateFixedSizeImage(temp,path,800,800,0.8));
+        	temp = null;
         }else{
-        	String thumb = generateScale(file,result,1f,0.8);
-        	if(thumb!=null){fileMap.put("thumbB", thumb);}
+        	result.put("medium", generateScale(file,path,1f,0.8));
         }          
-        return fileMap; 
+        return result; 
     }
 	
 	
 	/**
-     * 功能描述：删除图片
-     * @param oldPicName 修改之前的文件名
-     */
-    public static void deleteFile(String oldPicName) {
-        // 根据配置文件获取服务器图片存放路径
-    	String saveFilePath = PropertiesUtils.getFileIO("savePicUrl", "properties/config.properties");
-        // 构建文件目录 
-        File file= new File(saveFilePath+"/"+oldPicName);
-        if (file.exists()) {file.delete();}
+	 * 功能描述：删除图片
+	 * @param imageName 待删除图片名
+	 */
+    public static void deleteImage(String imageName) {
+        File imageFile= new File(IMAGE_PREFIX+"/"+imageName);
+        if (imageFile.exists()) {imageFile.delete();}
     }
 
     
     /**
      * 使用给定的图片生成指定大小的图片
      * @param file
-     * @param result
+     * @param imageName
      * @param width
      * @param height
      * @return
      */
-    private static String generateFixedSizeImage(MultipartFile file, String result, 
+    private static String generateFixedSizeImage(MultipartFile file, String path,
     		int width, int height, double quality){
-        try {
-            Thumbnails.of(file.getInputStream()).
+        try {    
+        	//拼接图片全路径
+        	String imageName = path + getImagePrefix(width,height) + "." 
+        					 + file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")+1);	
+            
+        	Thumbnails.of(file.getInputStream()).
             			size(width,height).
             			outputQuality(quality).
-            			toFile(new File(result));
-            return result;
+            			toFile(new File(IMAGE_PREFIX+imageName));
+            return imageName;
         } catch (IOException e) {
         	logger.error("generateFixedSizeImage",e);
             return null;
@@ -126,23 +115,36 @@ public class ImageUtils {
     /**
      * 按比例缩放图片
      * @param file
-     * @param result
+     * @param imageName
      * @param scale
      * @param quality
      * @return
      */
-    private static String generateScale(MultipartFile file, String result, 
-    		double scale, double quality){    	
+    private static String generateScale(MultipartFile file, String path, double scale, double quality){    	
         try {
+        	//拼接图片全路径
+        	String imageName = path + DateUtils.formatDate(new Date(), "yyyyMMddhhmmss") + ((int)(Math.random()*(99999-10000)+10000)) + "." 
+					 + file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")+1);	
+        	
             Thumbnails.of(file.getInputStream()).
                     scale(scale). // 图片缩放80%, 不能和size()一起使用
                     outputQuality(quality). // 图片质量压缩80%
-                    toFile(new File(result));   
-            return result;
+                    toFile(new File(IMAGE_PREFIX+imageName));   
+            return imageName;
         } catch (IOException e) {
             logger.error("generateFixedSizeImage",e);
             return null;
         }
+    }
+    
+    /**
+     * 生成图片前缀策略
+     * @param width
+     * @param height
+     * @return
+     */
+    private static String getImagePrefix(int width, int height){
+    	return DateUtils.formatDate(new Date(), "yyyyMMddhhmmss") + ((int)(Math.random()*(99999-10000)+10000)) + "_" + width + "_" +height;   
     }
     
     
