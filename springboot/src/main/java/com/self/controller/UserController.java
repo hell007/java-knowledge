a577a1fd53437911c11618bd3f3403e85ac2582b
hell007 <1048523306@qq.com>
2018-04-12 17:40:58 +0800

    Update and rename EmployeeController.java to UserController.java

---
+++ springboot/src/main/java/com/self/controller/UserController.java
@@ -0,0 +1,53 @@
+
+package com.self.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.self.model.User;
+import com.self.service.UserService;
+
+
+
+@Controller
+@RequestMapping(value = "/user")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	private User user;
+    
+	
+	@GetMapping("/index")
+	public String index(Model modle,User user,
+    		@RequestParam(name = "pageNum", defaultValue = "1") int pageNum,
+			@RequestParam(name = "pageSize", defaultValue = "10") int pageSize){
+		List<User> list = userService.selectPage(user,pageNum, pageSize);
+		//modle.addAttribute("page", new PageInfo<User>(list));
+		modle.addAttribute("list", list);
+		return "user/index";
+	}
+	
+
+    @ResponseBody
+    @RequestMapping("/list")
+    public Map<String,Object> list(
+    		@RequestParam(name = "pageNum", defaultValue = "1") int pageNum,
+			@RequestParam(name = "pageSize", defaultValue = "10") int pageSize){  
+    	Map<String,Object> map = new HashMap<String,Object>();
+    	List<User> list = userService.selectPage(user,pageNum, pageSize); 
+    	map.put("list",list);
+    	return map;   
+    }
+    
+    
+}
