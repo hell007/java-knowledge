78a09f2f6011932f1c6a2ac0899a084263a6ffdb
hell007 <1048523306@qq.com>
2018-04-12 15:53:50 +0800

    Update Application.java

--- hotdeploy/src/main/java/com/self/Application.java
+++ hotdeploy/src/main/java/com/self/Application.java
@@ -1,23 +1,14 @@
+package com.self;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-/**
- * @title Spring Boot 热启动
- * @describe 启动类
- * @author zc
- * @version 1.0 2017-12-01
- */
+
+
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-	
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
 }
