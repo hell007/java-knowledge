33b92e618b131007f2b1f0a17776ebf56f4c2fd6
hell007 <1048523306@qq.com>
2018-04-17 11:27:06 +0800

    Create ScheduleTask2.java

---
+++ springboot/src/main/java/com/self/task/ScheduleTask2.java
@@ -0,0 +1,50 @@
+package com.self.task;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.self.model.User;
+import com.self.service.UserService;
+
+
+/**
+ * 定时任务实现类（消息发送提醒）
+ */
+@DisallowConcurrentExecution
+public class ScheduleTask2 implements Job {
+	
+    private static Logger logger = LoggerFactory.getLogger(ScheduleTask1.class);
+    
+    @Autowired
+    private  UserService userService;
+
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+    	
+    	// 定义日期格式
+    	Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println("当前时间 : " + simpleDateFormat.format(calendar.getTime()));
+        
+        logger.info("==== （消息发送提醒）ScheduleTask2 ====> 开启!" + simpleDateFormat.format(calendar.getTime()));
+        try {
+
+        	List<User> userList = userService.select(null);
+			for (User user : userList) {
+				logger.info("do something"+user.getUsername()+"message消息");
+			}
+            
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+    }
+}
