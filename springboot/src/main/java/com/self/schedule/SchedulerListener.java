a6875dbca1a4aab89eb0aab312c5b3a536fcb7b4
hell007 <1048523306@qq.com>
2018-04-18 11:27:28 +0800

    Update SchedulerListener.java

--- springboot/src/main/java/com/self/schedule/SchedulerListener.java
+++ springboot/src/main/java/com/self/schedule/SchedulerListener.java
@@ -1,6 +1,7 @@
 package com.self.schedule;
 
 
+import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -19,11 +20,38 @@ import org.springframework.stereotype.Component;
 @Component
 public class SchedulerListener {
 	
+	/**
+	 * Spring boot 集成Quartz的使用（解决quartz的job无法注入spring对象的问题）
+	 * https://blog.csdn.net/mengruobaobao/article/details/79106343
+	 */
+	
 	@Autowired  
     public SchedulerAllJob myScheduler;  
 	
-	
-	/** 
+	@Autowired  
+    private JobFactory jobFactory;
+	  
+      
+    @Bean  
+    public SchedulerFactoryBean schedulerFactoryBean(){  
+        SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean(); 
+        schedulerFactoryBean.setJobFactory(jobFactory);
+        return schedulerFactoryBean;   
+    }
+    
+    /**
+     * 注入了一个 自定义的JobFactory ，然后 把其设置为SchedulerFactoryBean 的 JobFactory。
+     * 其目的是因为我在具体的Job 中 需要Spring 注入一些Service。
+     * 所以我们要自定义一个jobfactory， 让其在具体job 类实例化时 使用Spring 的API 来进行依赖注入
+     * @return
+     */
+    @Bean  
+    public Scheduler scheduler() {  
+        return schedulerFactoryBean().getScheduler();  
+    }  
+    
+    
+    /** 
      *  启动的时候执行该方法，或者是使用ApplicationListener，在启动的时候执行该方法 
      *  具体使用见：http://blog.csdn.net/liuchuanhong1/article/details/77568187 
      * @throws SchedulerException 
@@ -33,12 +61,6 @@ public class SchedulerListener {
     //@Scheduled(cron="0 08 18 ? * *")
     public void schedule() throws SchedulerException {   
             myScheduler.scheduleJobs();  
-     }   
-      
-    @Bean  
-    public SchedulerFactoryBean schedulerFactoryBean(){  
-        SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();   
-        return schedulerFactoryBean;   
-    }
+     } 
     
 }
