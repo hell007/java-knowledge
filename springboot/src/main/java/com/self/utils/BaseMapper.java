c7cfadff770a3fbfb5a490f2ad3178da6631529d
hell007 <1048523306@qq.com>
2018-04-20 10:23:03 +0800

    Delete BaseMapper.java

--- springboot/src/main/java/com/self/utils/BaseMapper.java
+++
@@ -1,7 +0,0 @@
-package com.self.utils;
-
-import tk.mybatis.mapper.common.Mapper;
-import tk.mybatis.mapper.common.MySqlMapper;
-
-public interface BaseMapper<T> extends Mapper<T>, MySqlMapper<T> {
-}
