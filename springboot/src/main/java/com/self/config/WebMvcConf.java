8e6df3aa3fd0a31a991ac2b2e63f0f8896c38d96
hell007 <1048523306@qq.com>
2018-06-22 11:12:19 +0800

    Create WebMvcConf.java

---
+++ springboot/src/main/java/com/self/config/WebMvcConf.java
@@ -0,0 +1,21 @@
+package com.self.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConf extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //将所有/static/** 访问都映射到classpath:/static/ 目录下
+        registry.addResourceHandler("/static/**")
+        .addResourceLocations("classpath:/static/");
+
+        registry.addResourceHandler("/webjars/**")
+        .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+        super.addResourceHandlers(registry);
+    }
+}
