70f44994726dfd6b0f1959b31e29d9774e2c09a7
午夜小白鼠 <1048523306@qq.com>
2020-01-07 15:06:18 +0800

    Create Room.java

---
+++ springboot/src/main/java/com/self/krpano/Room.java
@@ -0,0 +1,124 @@
+package com.self.krpano;
+
+public class Room {
+    private String roomid;
+
+    private String imgid;
+
+    private String name;
+
+    private String style;
+
+    private String area;
+
+    private Integer roomnum;
+
+    private Integer view;
+
+    private String time;
+    
+    private String intro;
+
+    private String type;
+
+    private String mark;
+    
+    private String status;
+
+    
+    public String getIntro() {
+        return intro;
+    }
+
+    public void setIntro(String intro) {
+        this.intro = intro == null ? null : intro.trim();
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type == null ? null : type.trim();
+    }
+    
+    public String getMark() {
+		return mark;
+	}
+
+	public void setMark(String mark) {
+		this.mark = mark;
+	}
+
+	public String getRoomid() {
+        return roomid;
+    }
+
+    public void setRoomid(String roomid) {
+        this.roomid = roomid == null ? null : roomid.trim();
+    }
+
+    public String getImgid() {
+        return imgid;
+    }
+
+    public void setImgid(String imgid) {
+        this.imgid = imgid == null ? null : imgid.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style == null ? null : style.trim();
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area == null ? null : area.trim();
+    }
+
+    public Integer getRoomnum() {
+        return roomnum;
+    }
+
+    public void setRoomnum(Integer roomnum) {
+        this.roomnum = roomnum;
+    }
+
+    public Integer getView() {
+        return view;
+    }
+
+    public void setView(Integer view) {
+        this.view = view;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time == null ? null : time.trim();
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+}
